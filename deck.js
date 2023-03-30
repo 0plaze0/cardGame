3cad81c9e4492c9e6454e8727e35cb6a1efccdc3
plaze <banikakash131@gamil.com>
2023-03-30 19:56:03 +0530

    Game Logic added

--- deck.js
+++ deck.js
@@ -24,7 +24,7 @@ export default class Deck{
     }
 }
 
-class Card{
+export class Card{
     constructor(suit,value){
         this.suit = suit;
         this.value = value;
--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import Deck from "./deck.js";
+import {Card} from "./deck.js";
+
+const CARD_DECK_VALUE  = {
+    "A":1, 
+    "2":2,
+    "3":3,
+    "4":4,
+    "5":5, 
+    "6":6,
+    "7":7,
+    "8":8,
+    "9":9,
+    "10":10,
+    "J":11,
+    "K":12,
+    "Q":13
+};
 
 const computerCardSlot = document.querySelector(".computer-card-slot");
 const playerCardSlot = document.querySelector(".player-card-slot");
@@ -10,9 +27,13 @@ const text = document.querySelector(".text");
 
 
 let playerDeck,computerDeck,inRound;
-startGame();
+
 
 document.addEventListener("click",()=>{
+    if(stop){
+        startGame();
+        return
+    }
     if(inRound){
         cleanUpBeforeRound();
     }else{
@@ -22,15 +43,18 @@ document.addEventListener("click",()=>{
 
 
 function startGame(){
+    stop=false;
     inRound=false;
     const newDeck = new Deck();
     newDeck.suffle();
 
-    const midPoint = Math.ceil(newDeck.lengthOfDeck/2);
+    const midPoint = Math.ceil((newDeck.lengthOfDeck)/2);
     playerDeck = new Deck(newDeck.cards.slice(0,midPoint-1));
     computerDeck = new Deck(newDeck.cards.slice(midPoint,newDeck.cards.lengthOfDeck));
-    // console.log(playerDeck);
-    // console.log(computerDeck);
+    // computerDeck = new Deck([new Card('s','0')]);
+    console.log(newDeck.cards.slice(midPoint+1,newDeck.cards.lengthOfDeck));
+    console.log(playerDeck);
+    console.log(computerDeck);
     cleanUpBeforeRound();
 }
 
@@ -49,14 +73,44 @@ function flipCard(){
     const playerCard = playerDeck.pop();
     const computerCard = computerDeck.pop();
 
-
     playerCardSlot.appendChild(playerCard.getHTML());
     computerCardSlot.appendChild(computerCard.getHTML());
 
     updateValue();
+
+    if(isRoundWinner(computerCard, playerCard)){
+        text.innerText = "lose";
+        playerDeck.push(playerCard);
+        playerDeck.push(computerCard);
+    }else if(isRoundWinner(playerCard, computerCard)){
+        text.innerText = "win";
+        computerDeck.push(playerCard);
+        computerDeck.push(computerCard);
+    }else {
+        text.innerText = "Draw";
+        playerDeck.push(playerCard);
+        computerDeck.push(computerCard);
+    }
+
+    if(isGameOver(playerDeck.lengthOfDeck)){
+        text.innerHTML = 'You lose';
+        stop = true;
+    }else if (isGameOver(computerDeck.length)){
+        text.innerHTML = 'You win';
+        stop = true;
+    }
+        
     
 }
 function updateValue(){
     computerDeckElem.innerHTML=computerDeck.lengthOfDeck;
     playerDeckElem.innerHTML=playerDeck.lengthOfDeck;
+}
+function isRoundWinner(cardOne, cardTwo){
+    // console.log(CARD_DECK_VALUE[cardOne.value]);
+    // console.log(CARD_DECK_VALUE[cardTwo.value]);
+    return CARD_DECK_VALUE[cardOne.value] > CARD_DECK_VALUE[cardTwo.value];
+}
+function isGameOver(deck){
+    return deck === 0;
 }
